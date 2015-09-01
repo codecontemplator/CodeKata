e76b7e09d679b1b0e79b57dd8edfc10b576adeab
codecontemplator <danielbrannstrom@hotmail.com>
2015-09-01 20:06:24 +0200

    .

---
+++ goat-wolf-salad/wgs.pl
@@ -0,0 +1,33 @@
+/** http://www.swi-prolog.org/pldoc/doc/swi/library/lists.pl */
+:- use_module(library(lists)).
+
+mover([],_,_,[]).
+mover(L,R,B,S) :-
+	append(L,B,L1),
+	member(X,L),
+	delete(L1,X,L2),
+	not(incompatible(L2)),
+	append([X],S2,S),
+	movel(L2,R,[X],S2).
+
+movel([],_,_,[]).
+movel(L,R,B,S) :-
+	append(R,B,R1),
+	(empty_return(L,R1,S); return_load(L,R,R1,S)).
+
+empty_return(L,R,S) :-
+	not(incompatible(R)),
+	append([empty],S2,S),
+	mover(L,R,[],S2).
+
+return_load(L,R,R1,S) :-
+	member(Y,R),
+	delete(R1,Y,R2),
+	not(incompatible(R2)),
+	append([Y],S2,S),
+	mover(L,R2,[Y],S2).
+
+incompatible([salad,goat]). 
+incompatible([goat,salad]). 
+incompatible([wolf,goat]). 
+incompatible([goat,wolf]). 
