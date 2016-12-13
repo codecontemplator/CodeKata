c298546e525dacf95ba55f1f51c7c3da60b5276c
Daniel Brännström <danielbrannstrom@hotmail.com>
2016-12-13 21:15:58 +0100

    Corrected compare function

--- five-problems/4.hs
+++ five-problems/4.hs
@@ -1,26 +1,12 @@
 -- http://www.shiftedup.com/2015/05/07/five-programming-problems-every-software-engineer-should-be-able-to-solve-in-less-than-1-hour
 
-import Data.Char
 import Data.List
 
 compareX :: Int -> Int -> Ordering
-compareX a b = compareX' (show a) (show b)
-    where
-    compareX' :: String -> String -> Ordering
-    compareX' [] [] = EQ
-    compareX' [] _ = LT
-    compareX' _ [] = GT
-    compareX' (x:xs) (y:ys) = 
-        let
-            x' = digitToInt x
-            y' = digitToInt y
-            r = compare x' y'
-        in
-            if r == EQ then
-                compareX' xs ys
-            else 
-                r
+compareX a b =compare (a' ++ b') (b' ++ a')
+    where 
+        a' = show a
+        b' = show b
 
 makeLargestNum :: [Int] -> Int
 makeLargestNum xs = read $ concat $ map show $ sortBy (flip compareX) xs 
-
